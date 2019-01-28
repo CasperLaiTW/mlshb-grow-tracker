475abc6e64dd1a88b68c78d3090420ee5aa2843d
Casper Lai <casper.lai@sleepingdesign.com>
2019-01-28 16:56:10 +0800

    Release

--- .postcssrc.js
+++ .postcssrc.js
@@ -4,12 +4,5 @@ module.exports = {
   plugins: [
     tailwindcss('./tailwind.js'),
     require('autoprefixer'),
-    //Only add purgecss in production
-    process.env.NODE_ENV === "production"? require("@fullhuman/postcss-purgecss")({
-      content: [
-        "./src/**/*.html",
-        "./src/**/*.vue"
-      ]
-    }): ""
   ]
 }
